5a59170ab8ba0af1c4f13d9900cb04da82ed1c8f
mariakatt <mariahkatt@gmail.com>
2024-12-06 19:23:50 +0100

    host

--- index.js
+++ index.js
@@ -35,7 +35,8 @@ app.use(express.json());
 app.use(cors());
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+  origin: 'https://bidibagsback-production.up.railway.app',
+    //origin: 'http://localhost:5173',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
   }));
 
