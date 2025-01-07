664ab763a097ff08b1fecb8d020ef2bc51e9c0b3
mariakatt <mariahkatt@gmail.com>
2025-01-07 18:39:14 +0100

    ultima revision

--- src/api/controllers/user.js
+++ src/api/controllers/user.js
@@ -93,8 +93,6 @@ const getUserById = async (req, res, next) => {
     try {
       const { id } = req.params;
       const user = await User.findById(id)
-      //.populate('favorites')
-      //.populate('cart')
       console.log(user)
       return res.status(200).json(user);
     } catch (error) {
